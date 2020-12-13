625b0426551ab2a986eb29f1318479cd4d677cf3
Andres Fernando <andres.fernando08@gmail.com>
2020-12-13 18:46:06 -0500

    manejo del useContext para carrito de compras

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Product from './Product';
+import AppContext from '../context/AppContext';
 import '../styles/components/Products.css';
 
-const Products = ({ products }) => (
-  <div className="Products">
-    <div className="Products-items">
-      {products.map((product) => (
-        <Product key={product.id} product={product} />
-      ))}
+const Products = () => {
+  // const { state, addToCart } = useContext(AppContext);
+  // const { products } = state;
+
+  // alternativa para destructurar objetos
+  const {
+    state: { products },
+    addToCart,
+  } = useContext(AppContext);
+
+  const handleAddToCart = (product) => {
+    addToCart(product);
+  };
+
+  return (
+    <div className="Products">
+      <div className="Products-items">
+        {products.map((product) => (
+          <Product
+            key={product.id}
+            product={product}
+            handleAddToCart={handleAddToCart}
+          />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Products;
