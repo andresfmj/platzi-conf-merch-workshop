f4937b523e2919aa3e1d239aadac130fa941c436
Andres Fernando <andres.fernando08@gmail.com>
2020-12-13 16:26:34 -0500

    layout e informacion de paginas

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import '../styles/components/Checkout.css';
 
-const Checkout = () => <h1>Checkout</h1>;
+const Checkout = () => (
+  <div className="Checkout">
+    <div className="Checkout-content">
+      <h3>Lista de pedidos:</h3>
+      <div className="Checkout-item">
+        <div className="Checkout-element">
+          <h4>ITEM NAME</h4>
+          <span>$10</span>
+        </div>
+        <button type="button">Eliminar</button>
+      </div>
+    </div>
+    <div className="Checkout-sidebar">
+      <h3>Precio total: $10</h3>
+      <button type="button">Continuar pedido</button>
+    </div>
+  </div>
+);
 
 export default Checkout;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Layout from '../components/Layout';
 
 import Checkout from '../containers/Checkout';
 import Home from '../containers/Home';
@@ -10,14 +11,16 @@ import SuccessPayment from '../containers/SuccessPayment';
 
 const App = () => (
   <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/checkout" component={Checkout} />
-      <Route exact path="/checkout/info" component={Information} />
-      <Route exact path="/checkout/payment" component={Payment} />
-      <Route exact path="/checkout/success" component={SuccessPayment} />
-      <Route component={NotFound} />
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/checkout" component={Checkout} />
+        <Route exact path="/checkout/info" component={Information} />
+        <Route exact path="/checkout/payment" component={Payment} />
+        <Route exact path="/checkout/success" component={SuccessPayment} />
+        <Route component={NotFound} />
+      </Switch>
+    </Layout>
   </BrowserRouter>
 );
 
