92ee151f1d271da77e12a60c5166003d225d1e95
Andres Fernando <andres.fernando08@gmail.com>
2020-12-12 22:27:09 -0500

    estructura de carpetas

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import '../styles/components/App.css';
+
+const hola = 'hola';
 
 const App = () => <h1>Hola React App</h1>;
 
--- src/index.js
+++ src/index.js
@@ -6,7 +6,4 @@ import { render } from 'react-dom';
 
 import App from './components/App';
 
-render(
-    <App />, 
-    document.getElementById('app')
-);
+render(<App />, document.getElementById('app'));
